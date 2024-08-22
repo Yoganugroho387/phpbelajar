1c3c73c93f650ef09d6ed8c00fd9de45248b528e
Yoga Nugroho <61351162+Yoganugroho387@users.noreply.github.com>
2024-08-23 00:30:50 +0700

    Add files via upload

---
+++ data.php
@@ -0,0 +1,55 @@
+<div id='preloader'></div>
+<div class='template-settings section' id='template-settings'><div class='widget LinkList' data-version='2' id='LinkList7'>
+<div class='belibeliwidget-title'>
+<h3 class='title'>
+PENGATURAN UMUM (Jangan ada yang dihapus)
+</h3>
+</div>
+<div class='widget-content'>
+<script>
+//<![CDATA[
+  let simpleBliSetting = {              
+"Auto Infinity Scroll":"off",
+"Default Rating":"5",
+"Harga Minimal Dapat Voucher":"100000",
+"Kode dan Harga Voucher":"Pstoremerakyat:10:off",
+"Link Profile":"https://",
+"Nomor WA Admin":"085963176460",
+"Teks Salam Chat Penjual":"Halo, saya mau nanya produk",
+"Teks Salam Pembuka Checkout WA":"Terimakasih sudah membeli produk kami, silahkan cek data dibawah ini:",
+}              
+//]]>
+</script>
+            </div>
+</div><div class='widget LinkList' data-version='2' id='LinkList100'>
+<div class='belibeliwidget-title'>
+<h3 class='title'>
+SETTING KONTAK FORM WA
+</h3>
+</div>
+<script>var kontakform_wa = [
+{"label":"Admin","name":"PSTORE:6285963176460"},
+];
+</script>
+</div><div class='widget LinkList' data-version='2' id='LinkList101'>
+<div class='belibeliwidget-title'>
+<h3 class='title'>
+SETTING BANK
+</h3>
+</div>
+<script>var bank = [
+{"name":"Via BRI","norek":" 1430 0100 3965 504 A/N RAHMAN KUSUMA DIPRAJA "},
+];
+</script>
+</div><div class='widget LinkList' data-version='2' id='LinkList102'>
+<div class='belibeliwidget-title'>
+<h3 class='title'>
+SETTING PILIHAN KURIR
+</h3>
+</div>
+<script>var optionkurir = [
+{"id":"JNE Experess","name":"JNE Experess"},
+{"id":"JNT ","name":"J&T EXPRESS "},
+{"id":"Go-Jek ","name":"GO-JEK "},
+];
+</script>
